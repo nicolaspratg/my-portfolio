831532b897e3193a048e82d183f2a184cfb20092
nicolaspratg <nicoprat@hotmail.com>
2024-05-20 17:10:38 -0300

    fixes to both nav styles

--- Components/MobileNav.tsx
+++ Components/MobileNav.tsx
@@ -10,9 +10,9 @@ const MobileNav = ({ nav, closeNav }: Props) => {
   const navAnimation = nav ? "translate-x-0" : "translate-x-[-100%]";
   return (
     <div
-      className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000] bg-black`}
+      className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000] bg-black`}
     >
-      <div className="w-[100vh] h-[100vh] flex flex-col items-center justify-center">
+      <div className="w-full h-full flex flex-col items-center justify-center">
         <div className="nav-link-mobile">HOME</div>
         <div className="nav-link-mobile">SERVICES</div>
         <div className="nav-link-mobile">ABOUT</div>
@@ -22,7 +22,7 @@ const MobileNav = ({ nav, closeNav }: Props) => {
       </div>
       <div
         onClick={closeNav}
-        className="absolute cursor-pointer top-[2rem] right-[2rem] w-[2rem] w-[2rem] text-[#831ea3]"
+        className="absolute cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-[#831ea3]"
       >
         <XMarkIcon />
       </div>
