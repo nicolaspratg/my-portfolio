0c0642d0503ae715ab1af1256a0d305b61f46330
nicolaspratg <nicoprat@hotmail.com>
2024-05-24 17:13:33 -0300

    Hero and about almost done

---
+++ Components/About.tsx
@@ -0,0 +1,47 @@
+import { ArrowDownTrayIcon } from "@heroicons/react/16/solid";
+import Image from "next/image";
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="pb-[3rem] pt-[4rem] md:pt-[8rem]">
+      <div className="grid grid-cols-1 md:grid-cols-2 w-[80%] mx-auto items-center h-[100%] gap-[3rem]">
+        <div>
+          <h1 className="text-[20px] mb-[1rem] text-white font-bold">
+            About me
+          </h1>
+          <h2 className="text-[25px] md:text-[35px] lg:text-[45px] md:leading-[3rem] leading-[2rem] capitalize mb-[3rem] text-white font-bold">
+            Transforming <span className="text-[#831ea3]">Visions</span>
+          </h2>
+          <div className="mb-[3rem] flex items-center md:space-x-10">
+            <span className="w-[100px] hidden md:block h-[5px] bg-slate-400 rounded-sm"></span>
+            <p className="text-[19px] text-slate-300 w-[80%]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              rerum vitae magnam! Numquam exercitationem soluta, nihil quam
+              voluptatibus non architecto sapiente adipisci error ipsa hic
+              commodi quasi incidunt. Hic, porro!
+            </p>
+          </div>
+          <a href="/files/CV.pdf" download="CV - Nicoas de Prat Gay.pdf">
+            <button className="px-[2rem] hover:bg-[#831ea3] transition-all duration-200 py-[1rem] text-[18px] font-bold text-white rounded bg-[#46374a59] border border-white flex items-center">
+              <p>Download my CV</p>
+              <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-white mx-[0.5rem]" />
+            </button>
+          </a>
+        </div>
+        <div className="lg:w-[500px] mx-auto md:mx-0 mt-[2rem] lg:mt-0 lg:h-[500px] w-[300px] h-[300px] relative">
+          <Image
+            src="/images/about.jpg"
+            alt="user"
+            layout="fill"
+            objectFit="contain"
+            className="relative z-[11] w-[100%] h-[100%] object-contain"
+          />
+          <div className="absolute z-[10] w-[100%] h-[100%] bg-[#831ea3] top-[-2rem] right-[-2rem]"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,16 +1,51 @@
 import React from "react";
 import Particle from "./Particle";
 import TextEffect from "./TextEffect";
+import Image from "next/image";
+import { ArrowDownTrayIcon } from "@heroicons/react/16/solid";
 
 const Hero = () => {
   return (
-    <div className="h-[88vh] bg-custom-gradient bg-cover bg-center">
+    <div className="h-[88vh]">
       <Particle />
-      <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[2rem] h-[100%] items-center">
-        <h1 className="text-[35px] md:text-[50px] text-white font-bold">
-          Hi, I'm <span className="text-[#831ea3]">Nicolás</span>
-        </h1>
-        <TextEffect />
+      <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[2rem] mt-[11vh] h-[100%] items-center">
+        <div>
+          <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+            Hi, I'm <span className="text-[#831ea3]">Nicolás</span>
+          </h1>
+          <TextEffect />
+          <p className="mt-[2rem] text-[18px] text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ad
+            temporibus blanditiis laborum quia, consectetur reprehenderit.
+            Ducimus, ut magnam aspernatur laboriosam, eligendi itaque ipsa
+            explicabo labore nam reprehenderit odio ex.
+          </p>
+          <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
+            <a href="/files/CV.pdf" download="CV - Nicoas de Prat Gay.pdf">
+              <button className="px-[2rem] hover:bg-[#831ea3] transition-all duration-200 py-[1rem] text-[18px] font-bold text-white rounded bg-[#46374a59] border border-white flex items-center">
+                <p>Download my CV</p>
+                <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-white mx-[0.5rem]" />
+              </button>
+            </a>
+            <button
+              onClick={() =>
+                window.open("https://github.com/nicolaspratg", "_blank")
+              }
+              className="px-[2rem] hover:bg-[#831ea3] transition-all duration-200 py-[1rem] text-[18px] font-bold text-white rounded bg-[#46374a59] border border-white flex items-center"
+            >
+              <p>Github</p>
+            </button>
+          </div>
+        </div>
+        <div className="w-[500px] hidden bg-[#831ea3] relative lg:flex items-center rounded-full h-[500px]">
+          <Image
+            src="/images/PPNico.jpg"
+            alt="user"
+            layout="fill"
+            className="object-cover rounded-full"
+            style={{ objectPosition: "center 20%" }}
+          />
+        </div>
       </div>
     </div>
   );
